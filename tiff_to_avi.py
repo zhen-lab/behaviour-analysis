a4132d49528520706042cff8964a719ae6b309d0
d2fong <d2fong@uwaterloo.ca>
2020-02-10 21:38:07 -0500

    fix bug where width and height were swapped in video writer args

--- tiff_to_avi.py
+++ tiff_to_avi.py
@@ -9,10 +9,16 @@ def sorted_tiff(f_path):
 tiff_files = glob.glob("./data/*.tiff")
 tiff_files.sort(key=sorted_tiff)
 
-images = [cv2.imread(file) for file in tiff_files]
+images = []
+
+for file in tiff_files:
+    img = cv2.imread(file)
+    # cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    images.append(img)
+
 width, height, _ = images[0].shape
 
-video = cv2.VideoWriter("./data/test.avi", cv2.VideoWriter_fourcc(*'XVID'), 26, (width, height))
+video = cv2.VideoWriter("./data/test.avi", cv2.VideoWriter_fourcc(*'XVID'), 26, (height, width))
 
 for img in images:
     video.write(img)
