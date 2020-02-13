a612ec2747047a78aacadbde6224025ac94a6877
d2fong <d2fong@uwaterloo.ca>
2020-02-13 13:36:31 -0500

    simplify image to avi problem code, add solution to the bottom of the file

--- tiff_to_avi_problem.py
+++ tiff_to_avi_problem.py
@@ -1,20 +1,65 @@
 import cv2
 import glob
 
-tiff_files = glob.glob("./data/*.tiff")
+path = './data/jpeg-30s/'
+img_extension = "*.jpg"
+fps = 10
 
-frames_per_second = 10
-images = [cv2.imread(file) for file in tiff_files]
+img_files = glob.glob(path + img_extension)
+
+images = [cv2.imread(file) for file in img_files]
+
 video_width, video_height, _ = images[0].shape
 
 video = cv2.VideoWriter(
-  "./data/test.avi",
+  path + "test.avi",
   cv2.VideoWriter_fourcc(*"XVID"),
   # -1,
-  frames_per_second,
-  (video_width, video_height))
+  fps,
+  (video_height, video_width))
 
 for image in images:
   video.write(image)
 
 video.release()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def sort_file_by_name_num(f_path):
+#     f_name = f_path.split('/')[-1]
+#     key = f_name.split('.')[0]
+#     return int(key)
+
+
+# img_files.sort(key=sort_file_by_name_num)
