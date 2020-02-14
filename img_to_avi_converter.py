081db14eed9f93d2a727609530a8fea80949b05c
Dylan Fong <dylanfong.ut@gmail.com>
2020-02-14 14:45:05 -0500

    fix typos in img converter

--- img_to_avi_converter.py
+++ img_to_avi_converter.py
@@ -9,12 +9,12 @@ def file_name_str_to_int(f_path):
 
 path = './data/jpeg-30s/'
 img_extension = "*.jpg"
-fps = 10
+frames_per_second = 10
 
-img_files = glob.glob(path + img_extension)
+img_files = sorted(glob.glob(path + img_extension))
 img_files.sort(key=file_name_str_to_int)
 
-images = [cv2.imread(file) for file in tiff_files]
+images = [cv2.imread(file) for file in img_files]
 width, height, _ = images[0].shape
 
 video = cv2.VideoWriter(path + 'test.avi', cv2.VideoWriter_fourcc(*'XVID'), frames_per_second, (height, width))
