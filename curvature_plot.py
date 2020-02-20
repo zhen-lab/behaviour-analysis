cc39c4c61466e490c03fc7c58fca3bf3927d2b11
Dylan Fong <dylanfong.ut@gmail.com>
2020-02-20 15:03:09 -0500

    rename hdf5 data to curvature_plot - add method to generate curvature plot from taizo's data

--- curvature_plot.py
+++ curvature_plot.py
@@ -1,7 +1,59 @@
+import glob
+import pandas as pd
+import numpy as np
 import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
-import numpy as np; np.random.seed(0)
+import h5py
 
-uniform_data = np.random.rand(10, 12)
-ax = sns.heatmap(uniform_data)
-plt.show()
+from tierpsy.features.tierpsy_features import get_curvature_features
+
+
+def curvature_plot_from_tierpsy_features_func():
+    with h5py.File('./data/jpeg-30s/Results/test_skeletons.hdf5') as f:
+        skeletons = np.array(f['skeleton'][:504])
+        segment_curvatures = get_curvature_features(skeletons, 'grad')
+        print(segment_curvatures)
+        data = { k: segment_curvatures[k] for k in segment_curvatures.keys() }
+        df = pd.DataFrame(data=data)
+        ax = sns.heatmap(df.transpose(), cmap=sns.diverging_palette(220, 20, n=3, as_cmap=True))
+        plt.show()
+
+
+def curvature_plot_from_tierpsy_hdf5():
+    f = pd.read_hdf('./data/jpeg-30s/Results/test_featuresN.hdf5', 'timeseries_data', 'r', columns=[
+        'curvature_head',
+        'curvature_neck',
+        'curvature_midbody',
+        'curvature_hips',
+        'curvature_tail',
+    ])
+
+
+    curvature_keys = [    
+        'curvature_head',
+        'curvature_neck',
+        'curvature_midbody',
+        'curvature_hips',
+        'curvature_tail'
+    ]
+
+    data = { k: f[k][:499] for k in curvature_keys }
+    df = pd.DataFrame(data=data)
+    ax = sns.heatmap(df.transpose(), center=0.0, cmap=sns.diverging_palette(220, 20, n=3, as_cmap=True))
+    plt.show()
+
+
+
+def curvature_plot_from_taizo_data():
+    f = pd.read_csv('./data/sample-curvature-data/temp1HR1.csv')
+    angle_columns = [ 'angle_' + str(i) for i in range(1, 34) ]
+
+    data = { k: f[k] for k in angle_columns }
+    df = pd.DataFrame(data=data)
+    ax = sns.heatmap(df.transpose(), center=0.0, cmap=sns.diverging_palette(220, 20, n=3, as_cmap=True))
+    plt.show()
+
+
+# curvature_plot_from_tierpsy_features_func()
+# curvature_plot_from_tierpsy_hdf5()
+curvature_plot_from_taizo_data()
