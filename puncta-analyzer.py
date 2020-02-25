9a77156da6b0df3378cdb638a77fec8942b7fb4e
Dylan Fong <dylanfong.ut@gmail.com>
2020-02-25 14:39:58 -0500

    tune puncta analyzer parameters - use more descriptive variables - add comments

--- puncta-analyzer.py
+++ puncta-analyzer.py
@@ -8,22 +8,25 @@ import matplotlib.pyplot as plt
 import tifffile
 
 puncta_img = tifffile.imread('./data/puncta/subtracted.tif')
-# puncta_img = sk_io.imread('./data/puncta/subtracted.jpg')
 
-puncta_img_gray = puncta_img
+# a puncta is a row of tuples containing (x, y, sigma)
+# where x, y is the pixel position in the image
+# where sigma is proprotional to the punctums radius
+puncta = feature.blob_log(puncta_img, max_sigma=5, overlap=0.9, threshold=.05)
 
-blobs_log = feature.blob_log(puncta_img_gray, max_sigma=30, num_sigma=10, threshold=.05)
+# Compute radii of each punctum
+# radius = sqrt(2) * sigma
+puncta[:, 2] = puncta[:, 2] * sqrt(2)
 
-# Compute radii in the 3rd column.
-blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
-
+# show the puncta image
 fig, ax = plt.subplots(1)
 ax.set_title('puncta')
-ax.imshow(puncta_img_gray, cmap=plt.cm.binary, aspect='auto')
+ax.imshow(np.invert(puncta_img), cmap=plt.cm.binary, aspect='auto')
 
-for blob in blobs_log:
-    y, x, r = blob
-    c = plt.Circle((x, y), r, color='blue', linewidth=2, fill=False)
-    ax.add_patch(c)
+# show where each recognized punctum is on the image
+for punctum in puncta:
+    y, x, r = punctum
+    punctum_marker = plt.Circle((x, y), r, color='blue', linewidth=2, fill=False)
+    ax.add_patch(punctum_marker)
 
 plt.show()
