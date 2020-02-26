2c8cdca3ab1c02edb2101cade4562ffdf57e704f
Dylan Fong <dylanfong.ut@gmail.com>
2020-02-26 10:16:38 -0500

    compute puncta stats, annotated images and save it to csv

--- puncta-analyzer.py
+++ puncta-analyzer.py
@@ -1,13 +1,19 @@
+import pandas as pd
 import numpy as np
 np.set_printoptions(threshold=np.inf)
+import matplotlib.pyplot as plt
+import tifffile
 
 from math import sqrt
 from skimage import feature
 from skimage.color import rgb2gray
-import matplotlib.pyplot as plt
-import tifffile
 
-puncta_img = tifffile.imread('./data/puncta/subtracted.tif')
+
+p = './data/puncta/'
+fname = 'subtracted'
+fname_ext = '.tif'
+
+puncta_img = tifffile.imread(p + fname + fname_ext)
 
 # a puncta is a row of tuples containing (x, y, sigma)
 # where x, y is the pixel position in the image
@@ -21,19 +27,30 @@ puncta[:, 2] = puncta[:, 2] * sqrt(2)
 # show the puncta image
 fig, ax = plt.subplots(1)
 ax.set_title('puncta')
-ax.imshow(np.invert(puncta_img), cmap=plt.cm.binary, aspect='auto')
+ax.imshow(np.invert(puncta_img), cmap=plt.cm.binary, aspect='equal')
 
 # show where each recognized punctum is on the image
 for punctum in puncta:
     y, x, r = punctum
-    print(puncta_img[int(y)][int(x)]) # pixel intensity of (x, y), note that y comes first in the img array
-    print(y, x)
     punctum_marker = plt.Circle((x, y), r, color='blue', linewidth=1, fill=False)
     ax.add_patch(punctum_marker)
 
-plt.show()
+# plt.show()
+plt.savefig(p + 'annotated-' + fname + '.png')
 
 # compute puncta statistics
+# save them as a csv
 # for each puncta get x, y, radius (approximate), area (approximate), distances ?, pixel intensity(x, y)
 
-# write to csv
+puncta_stats_columns = ['x', 'y', 'radius (approximation)', 'area (approximation)', 'pixel intensity']
+puncta_stats = []
+for punctum in puncta:
+    y, x, r = punctum
+    punctum_pixel_intensity = puncta_img[int(y)][int(x)]
+    area = ( r ** 2 ) * np.pi
+    puncta_stats.append([x, y, r, area, punctum_pixel_intensity])
+
+numpy_puncta_stats = np.array(puncta_stats)
+pandas_punca_stats = pd.DataFrame(data=numpy_puncta_stats, columns=puncta_stats_columns)
+
+pandas_punca_stats.to_csv(p + 'puncta-stats.csv')
