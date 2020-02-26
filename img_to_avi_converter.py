07fc5f694c16a20879e349cf3f1341c580ee0995
Dylan Fong <dylanfong.ut@gmail.com>
2020-02-26 14:45:37 -0500

    put video file name into variable

--- img_to_avi_converter.py
+++ img_to_avi_converter.py
@@ -10,6 +10,7 @@ def file_name_str_to_int(f_path):
 path = './data/jpeg-30s/'
 img_extension = "*.jpg"
 frames_per_second = 10
+video_file_name = path + 'test.avi'
 
 img_files = sorted(glob.glob(path + img_extension))
 img_files.sort(key=file_name_str_to_int)
@@ -17,7 +18,7 @@ img_files.sort(key=file_name_str_to_int)
 images = [cv2.imread(file) for file in img_files]
 width, height, _ = images[0].shape
 
-video = cv2.VideoWriter(path + 'test.avi', cv2.VideoWriter_fourcc(*'XVID'), frames_per_second, (height, width))
+video = cv2.VideoWriter(video_file_name, cv2.VideoWriter_fourcc(*'XVID'), frames_per_second, (height, width))
 
 for img in images:
     video.write(img)
